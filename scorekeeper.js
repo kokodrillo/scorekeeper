ed623328ed74d456b818c65292f5c32058638fea
Maja Polanc <mayainstudio@gmail.com>
2020-01-07 19:31:20 +0100

    setting js

---
+++ scorekeeper.js
@@ -0,0 +1,9 @@
+let p1Button = document.getElemebtById("#p1");
+let p2Button = document.getElementById("#p2");
+let p1Display = document.getElementById("#display1");
+let p2Display = document.getElementById("#display2");
+
+let p1Score = 0;
+let p2Score =0;
+let gameOver = false;
+let WinningScore = 5;
